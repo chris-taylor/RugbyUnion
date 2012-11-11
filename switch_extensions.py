0f6ba366840db43045bff33118df5918e66db5ca
Chris Taylor <crntaylor@gmail.com>
2012-11-11 00:40:07 +0000

    recurse into subdirectories

--- switch_extensions.py
+++ switch_extensions.py
@@ -2,17 +2,26 @@ import os
 import sys
 
 def switch_extensions(efrom,eto):
+    ignore = ['.git']
+
     contents = os.listdir('.')
 
-    for f in contents:
-        switch(f,efrom,eto)
+    for obj in contents:
+        if obj in ignore:
+            continue
+        elif os.path.isfile(obj):
+            switch(obj,efrom,eto)
+        elif os.path.isdir(obj):
+            os.chdir(obj)
+            switch_extensions(efrom,eto)
+            os.chdir('..')
 
 def switch(f,efrom,eto):
     try:
         name, ext = f.split('.')
     except:
         return
-        
+
     if ext == efrom:
         os.rename(f, name + '.' + eto)
 
